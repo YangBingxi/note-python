a6988e7d8e865f58eab0cf020cc4f6b9fd291826
youngsw <sw_young@outlook.com>
2020-03-05 16:27:17 +0800

    add tool

---
+++ tools/magic.py
@@ -0,0 +1,6 @@
+# when you run python command in terminal,you can add %before the command, it like you run linux command in python
+# like this: %ls
+# %mkdir magic
+# %touch magic
+# %lsmagic
+# ...
